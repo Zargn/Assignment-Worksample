5fdd5c9adb65a59ec1a92a5619fd16c7cb5b6e39
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-24 14:33:28 +0200

    Configured default request.

--- Tiny Browser/TinyBrowser.cs
+++ Tiny Browser/TinyBrowser.cs
@@ -10,7 +10,7 @@ public class Program { public static void Main() { new TinyBrowser().Run(); } }
 
 public class TinyBrowser
 {
-    private const string DefaultRequest = "NOT IMPLEMENTED!";
+    private const string DefaultRequest = "GET / HTTP/1.1\r\nHost: www.acme.com\r\n\r\n"; //"{command} {uri} {protocol-version}\r\n{header-key}: {header-value}\r\n{header-key}: {header-value}\r\n\r\n"
     
     public void Run()
     {
@@ -18,8 +18,8 @@ public class TinyBrowser
         TinyHttpClient tinyHttpClient = new();
         var hostname = AskUserForStringInput("Please enter hostname");
         var port = AskUserForIntegerInput("Please enter port number");
-        var request = AskUserForStringInput("Please enter http request");
-        tinyHttpClient.SendHttpRequest(hostname, port, request);
+        // var request = AskUserForStringInput("Please enter http request");
+        tinyHttpClient.SendHttpRequest(hostname, port, DefaultRequest);
     }
 
     
