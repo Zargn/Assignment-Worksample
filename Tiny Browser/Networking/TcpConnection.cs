f55c29430555865f98e59796e1fc978d04c96c21
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-24 14:56:05 +0200

    Create TcpConnection.cs

---
+++ Tiny Browser/Networking/TcpConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+
+namespace Tiny_Browser.Networking;
+
+public class TcpConnection
+{
+    public event Action<string> OnMessageReceive;
+
+    private TcpClient tcpClient;
+    private StreamWriter streamWriter;
+    private StreamReader streamReader;
+    
+    public TcpConnection(string hostname, int port)
+    {
+        tcpClient = new TcpClient(hostname, port);
+        streamWriter = new StreamWriter(tcpClient.GetStream());
+        streamReader = new StreamReader(tcpClient.GetStream());
+    }
+
+    public void SendMessage(string request)
+    {
+        streamWriter.WriteLine(request);
+        streamWriter.Flush();
+    }
+
+    public async Task<string?> WaitForResponse()
+    {
+        var response = await streamReader.ReadLineAsync();
+        return response;
+    }
+}
