989ac7f5ecf7b643cc9de3fa32017e615fa4788f
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-24 12:38:37 +0200

    Started work on the user input for tinybrowser

---
+++ Tiny Browser/Networking/TinyHttpClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Tiny_Browser.Networking;
+
+public class TinyHttpClient
+{
+    public string SendHttpRequest(string hostname, int port, string request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tiny Browser/TinyBrowser.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Tiny_Browser.Networking;
+
+namespace Tiny_Browser;
+
+public class Program { public static void Main() { new TinyBrowser().Run(); } }
+
+
+
+public class TinyBrowser
+{
+    private const string DefaultRequest = "NOT IMPLEMENTED!";
+    
+    public void Run()
+    {
+        Console.WriteLine("Hello world!");
+        TinyHttpClient tinyHttpClient = new();
+        var hostname = AskUserForStringInput("Please enter hostname");
+        var port = AskUserForIntegerInput("Please enter port number");
+        var request = AskUserForStringInput("Please enter http request");
+        tinyHttpClient.SendHttpRequest(hostname, port, request);
+    }
+
+    
+    
+    private string AskUserForStringInput(string question)
+    {
+        Console.WriteLine(question + " :");
+        return Console.ReadLine();
+    }
+
+    private int AskUserForIntegerInput(string question)
+    {
+        while (true)
+        {
+            try
+            {
+                var inputString = AskUserForStringInput(question);
+                var inputInteger = Int32.Parse(inputString);
+                return inputInteger;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect format! Only numbers are allowed. Please try again.");
+            }
+        }
+    }
+}
