9a30e43381b96036a991218b782bb103d01c2bd2
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-25 14:22:38 +0200

    Implemented http request class based on lecture

---
+++ Tiny Browser/Networking/Http11Request.cs
@@ -0,0 +1,23 @@
+﻿namespace Tiny_Browser.Networking;
+
+public class Http11Request
+{
+    public const string EndOfHttpRequest = "\n\n";
+    public string Method;
+    public string Path;
+    public Dictionary<string, string> Headers = new();
+
+    public Http11Request(string method, string path, string hostName)
+    {
+        Method = method;
+        Path = path;
+        Headers["Connection"] = "Keep-Alive";
+        Headers["Host"] = hostName;
+    }
+
+    public override string ToString()
+    {
+        return
+            $"{Method} {Path} HTTP/1.1\n{string.Join("\n", Headers.Select(pair => $"{pair.Key}: {pair.Value}"))}{EndOfHttpRequest}";
+    }
+}
--- Tiny Browser/Networking/TinyHttpClient.cs
+++ Tiny Browser/Networking/TinyHttpClient.cs
@@ -2,14 +2,11 @@
 
 public class TinyHttpClient
 {
-    private const string DefaultRequest = "GET / HTTP/1.1\r\nHost: www.acme.com\r\n\r\n"; //"{command} {uri} {protocol-version}\r\n{header-key}: {header-value}\r\n{header-key}: {header-value}\r\n\r\n"
-
-    private string GetRequest(string hostname) => $"GET / HTTP/1.1\r\nHost: {hostname}\r\n\r\n";
-
     public string? SendHttpRequest(string hostname, int port)
     {
         TcpConnection tcpConnection = new TcpConnection(hostname, port);
-        tcpConnection.SendMessage(GetRequest(hostname));
+        // tcpConnection.SendMessage(GetRequest(hostname));
+        tcpConnection.SendMessage(new Http11Request("GET", "/", hostname).ToString());
 
         var receive = tcpConnection.WaitForResponse();
 
