718d0a55e4c8d505c8c0733bb1383429e10ffd6a
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-31 15:47:53 +0200

    Fix shared projects not having gitignores

--- TinyHtmlReader/ITagHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TinyHtmlReader;
-
-public interface ITagHandler
-{
-    public HtmlObjectBase GetObjectFromString(string objectString);
-    public string Tag { get; }
-    public string EndTag { get; }
-}
