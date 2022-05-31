76e706f43cad11f3a7732cb33ccde34e07c55e44
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-05-31 15:48:50 +0200

    Added back shared projects

---
+++ TinyHtmlReader/HtmlObjectBase.cs
@@ -0,0 +1,11 @@
+﻿namespace TinyHtmlReader;
+
+public abstract class HtmlObjectBase
+{
+    public readonly string Type;
+
+    public HtmlObjectBase()
+    {
+        Type = GetType().FullName;
+    }
+}
---
+++ TinyHtmlReader/ITagHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TinyHtmlReader;
+
+public interface ITagHandler
+{
+    public HtmlObjectBase GetObjectFromString(string objectString);
+    public string Tag { get; }
+    public string EndTag { get; }
+}
