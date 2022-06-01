a9ffe0e15c46357f652f1327b2da19ddfa269c2c
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-01 11:17:41 +0200

    Formatted githubexplorer script

---
+++ GithubExplorer/GithubExplorer.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleUtils;
+
+namespace GithubExplorer;
+
+public class Program { public static void Main() { new GithubExplorer().Run(); } }
+
+public class GithubExplorer
+{
+    public void Run()
+    {
+        var targetUser = Utility.AskUserForStringInput(@"Welcome to this very limited github browser!
+Please enter a user you like to inspect!");
+
+        Console.WriteLine(targetUser);
+    }
+}
