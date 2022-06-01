f04f7c2036f8373accfcaddbe57cf2fe8c671120
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-01 16:35:32 +0200

    Completed GetUser method.
    Returns a user object with correct name and location

--- GithubExplorer/Networking/GithubAPI.cs
+++ GithubExplorer/Networking/GithubAPI.cs
@@ -1,4 +1,6 @@
-﻿using GithubExplorer.Interfaces;
+﻿using System.Text.Json;
+using GithubExplorer.GithubObjects;
+using GithubExplorer.Interfaces;
 
 namespace GithubExplorer.Networking;
 
@@ -12,12 +14,17 @@ public class GithubAPI : IGithubAPI
     }
 
     private IHttpClient httpClient;
+    private readonly JsonSerializerOptions serializeAllFields = new() {IncludeFields = true};
     
     public IUser GetUser(string userName)
     {
         HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"users/{userName}");
         var response = httpClient.SendRequest(request);
-        Console.WriteLine(response);
-        throw new NotImplementedException();
+
+        var json = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+
+        var user = JsonSerializer.Deserialize<User>(json, serializeAllFields);
+
+        return user;
     }
 }
