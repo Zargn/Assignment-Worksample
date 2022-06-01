56edf6874f9bd1a54aa3687baa33057b25a32faf
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-01 17:17:17 +0200

    Created httpresponsemessage extension method
    Lets us directly deserialize the message into the wanted type

---
+++ GithubExplorer/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using GithubExplorer.GithubObjects;
+
+namespace GithubExplorer;
+
+public static class ExtensionMethods
+{
+    public static T Deserialize<T>(this HttpResponseMessage httpResponseMessage)
+    {
+        var json = new StreamReader(httpResponseMessage.Content.ReadAsStream()).ReadToEnd();
+        var result = JsonSerializer.Deserialize<T>(json);
+        return result;
+    }
+}
