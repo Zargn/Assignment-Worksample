85cd29dcd24a4666133d8b438454340547a40d03
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-01 14:48:07 +0200

    Created HttpConnection class & interface

---
+++ GithubExplorer/Interfaces/IHttpClient.cs
@@ -0,0 +1,14 @@
+﻿namespace GithubExplorer.Interfaces;
+
+
+
+public interface IHttpClient
+{
+    public HttpResponseMessage SendRequest(HttpRequestMessage httpRequestMessage);
+
+    public void Dispose();
+
+    public void AddDefaultHeaders(string header, string value);
+
+    public void RemoveDefaultHeaders(string header);
+}
---
+++ GithubExplorer/Networking/HttpConnection.cs
@@ -0,0 +1,35 @@
+﻿using GithubExplorer.Interfaces;
+
+namespace GithubExplorer.Networking;
+
+public class HttpConnection : IHttpClient
+{
+    // HttpClient is intended to be instantiated once per application, rather than per-use. See microsoft docs.
+    static readonly HttpClient httpClient = new();
+
+    public HttpConnection()
+    {
+        httpClient.BaseAddress = new Uri("https://api.github.com");
+    }
+
+    public HttpResponseMessage SendRequest(HttpRequestMessage httpRequestMessage)
+    {
+        var response = httpClient.Send(httpRequestMessage);
+        return response;
+    }
+
+    public void Dispose()
+    {
+        httpClient.Dispose();
+    }
+
+    public void AddDefaultHeaders(string header, string value)
+    {
+        httpClient.DefaultRequestHeaders.Add(header, value);
+    }
+
+    public void RemoveDefaultHeaders(string header)
+    {
+        httpClient.DefaultRequestHeaders.Remove(header);
+    }
+}
