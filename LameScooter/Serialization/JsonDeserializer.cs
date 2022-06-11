41d37764d4c421a68806986eb1742a110190fa8c
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-11 17:10:17 +0200

    Create JsonDeserializer.cs

---
+++ LameScooter/Serialization/JsonDeserializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+using LameScooter.Interfaces;
+
+namespace LameScooter.Serialization;
+
+public class JsonDeserializer : IJsonSerializer
+{
+    public T DeSerialize<T>(string json)
+    {
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
