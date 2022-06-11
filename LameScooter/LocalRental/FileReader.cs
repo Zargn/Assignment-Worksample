8b3b37329ff0caaff98715b6ce2e41a11ed15400
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-11 17:38:49 +0200

    Create FileReader.cs

---
+++ LameScooter/LocalRental/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using LameScooter.Interfaces;
+
+namespace LameScooter.LocalRental;
+
+public class FileReader : IFileReader
+{
+    public async Task<string> ReadFileAsync(string filePath)
+    {
+        var fileStream = new FileStream(filePath, FileMode.Open);
+        var streamReader = new StreamReader(fileStream);
+        
+        return await streamReader.ReadToEndAsync();
+    }
+}
