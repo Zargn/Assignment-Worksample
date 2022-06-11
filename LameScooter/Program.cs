1bf584283b5ca2d9cbdc62597270405087dbe904
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-11 17:36:12 +0200

    Updated user interface code

---
+++ LameScooter/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleUtils;
+using LameScooter.Interfaces;
+using LameScooter.Serialization;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        ILameScooterRental scooterRental = new LocalLameScooterRental(new JsonDeserializer());
+        
+        var stationName = Utility.AskUserForStringInput("Please enter station name to get statistics from: ");
+        var amount = await scooterRental.GetAvailableScootersAtStation(stationName);
+        Console.WriteLine(amount);
+        if (await scooterRental.TryGetStation(stationName, out IStationModel stationModel))
+        {
+            Console.WriteLine(stationModel);
+        }
+        else
+        {
+            Console.WriteLine("Station not found!");
+        }
+    }
+}
