b84acd79cb5d646a6b8f2c315bdd0c0b11ee9706
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-11 17:10:28 +0200

    Create StationModel.cs

---
+++ LameScooter/Data/StationModel.cs
@@ -0,0 +1,21 @@
+﻿using LameScooter.Interfaces;
+
+namespace LameScooter.Data;
+
+public record StationModel : IStationModel
+{
+    public string id { get; }
+    public string name { get; }
+    public int x { get; }
+    public int y { get; }
+    public int bikesAvailable { get; }
+    public int spacesAvailable { get; }
+    public int capacity { get; }
+    public bool allowDropoff { get; }
+    public bool allowOverloading { get; }
+    public bool isFloatingBike { get; }
+    public bool isCarStation { get; }
+    public string state { get; }
+    public string[] networks { get; }
+    public bool realTimeData { get; }
+}
