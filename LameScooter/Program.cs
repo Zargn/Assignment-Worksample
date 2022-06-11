a3c636eb23a0e2d693768e879e7056e78b7af35e
Oskar Anderson <oskar.anderson@student.forsbergsskola.se>
2022-06-11 18:08:57 +0200

    Configure main program to use local rental

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,12 +1,13 @@
 ﻿using ConsoleUtils;
 using LameScooter.Interfaces;
+using LameScooter.LocalRental;
 using LameScooter.Serialization;
 
 public class Program
 {
     static async Task Main(string[] args)
     {
-        ILameScooterRental scooterRental = new LocalLameScooterRental(new JsonDeserializer());
+        ILameScooterRental scooterRental = new LocalLameScooterRental(new JsonDeserializer(), new FileReader());
         
         var stationName = Utility.AskUserForStringInput("Please enter station name to get statistics from: ");
         var amount = await scooterRental.GetAvailableScootersAtStation(stationName);
